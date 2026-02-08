10f587327466cdec6f791ce8f773abfe1b3c41c3
Tejas Prajapati <prajapatitejas91@gmail.com>
2026-02-08 12:57:12 +0530

    sdio 18v quirk
    Signed-off-by: Tejas Prajapati <prajapatitejas91@gmail.com>

--- drivers/mmc/core/quirks.h
+++ drivers/mmc/core/quirks.h
@@ -207,6 +207,10 @@ static const struct mmc_fixup __maybe_unused sdio_card_init_methods[] = {
 			      MMC_QUIRK_LENIENT_FN0 |
 			      MMC_QUIRK_BLKSZ_FOR_BYTE_MODE),
 
+	SDIO_FIXUP_COMPATIBLE("morse,mm6104", add_quirk, MMC_QUIRK_NO_18V),
+
+	SDIO_FIXUP_COMPATIBLE("morse,mm610x", add_quirk, MMC_QUIRK_NO_18V),
+
 	END_FIXUP
 };
 
